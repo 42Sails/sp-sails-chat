91733bc6c6b1fa4d4e2542890153dfd14c280969
Your Name <you@example.com>
2019-09-14 22:43:40 +0500

    fix user

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -9,9 +9,8 @@ module.exports = {
 
   render: async (request, response) => {
     try {
-      let data = await User.findOne({
-        uuid: 'aywevr'
-      });
+      let data = await User.findOne({id:request.session.userId});
+
       if (!data) {
         return response.notFound('The user was NOT found!');
       }
