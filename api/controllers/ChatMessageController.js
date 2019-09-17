6f4710fd1e938b8839890d0c11dfd61b5d79ab81
Your Name <you@example.com>
2019-09-17 21:04:39 +0500

    some fix

--- api/controllers/ChatMessageController.js
+++ api/controllers/ChatMessageController.js
@@ -49,21 +49,20 @@ module.exports = {
 // 		return response.view('chatroom');
 // 	},
 
+
 	postMessage: async (request, response) => {
-		// Make sure this is a socket request (not traditional HTTP)
-    if (!request.isSocket) {
-      return response.badRequest();
-    }
+
 
 		try {
-			let user = await User.findOne({uuid:request.body.uuid});
-			let msg = await ChatMessage.create({message:request.body.message, createdBy:user.id });
+
+			let user = await User.findOne({uuid: request.body.uuid });
+			console.log(user);
+			let msg = await ChatMessage.create({message:request.body.message, createdBy:user.id  });
 			if(!msg.id) {
 				throw new Error('Message processing failed!');
 			}
-			msg.createdBy = user.id;
-			//ChatMessage.publishCreate(msg);
-			sails.sockets.broadcast("chatmessage", "test", msg);
+			msg.createdBy = user.id ;
+			ChatMessage.publishCreate(msg);
 		} catch(err) {
 			return response.serverError(err);
 		}
